794ce7d2c44a199d6c5b40db114b7f4d10ca7a7d
Masterflash <sxq@yandex.ru>
2018-03-12 10:46:52 +0300

    f

--- src/Lib/Func/GetLocales.php
+++ src/Lib/Func/GetLocales.php
@@ -14,10 +14,10 @@ public function __invoke($obj,$infa,$struct_arr,$pole_type,$pole_dop,$tab_name,$
 		//подменить список
 		$obj->dop_sql['name']=$l;
 		$obj->dop_sql['id']=$l;
-		//это значение по умолчанию
+		//это значение по умолчанию 
 	if  (!$obj->pole_dop[1]) 
 		{
-			$obj->pole_dop[1]=$obj->dop_sql['id'][1];
+			$obj->pole_dop[1]=$obj->dop_sql['id'][0];
 			$obj->pole_dop1=$obj->pole_dop[1];
 			}
 	return $infa;
